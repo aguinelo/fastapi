e9f549f6953e7586dce1e1c87ba02ff6ba9418fb
Aguinelo Koczkodai <aguinelo@gmail.com>
2020-11-04 16:48:54 -0300

    feature CORS config

--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+# CORS handler
+origins = [
+    "http://localhost.aguinelo.com",
+    "https://localhost.aguinelo.com",
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 async def main():
